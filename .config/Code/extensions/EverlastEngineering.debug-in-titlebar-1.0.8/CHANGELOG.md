d03c33ea6226b7dd2e75dd39b59c83f66566801d
egirlcatnip <egirlcatnip@gmail.com>
2025-02-06 17:18:25 +0100

    Updated Codium

--- .config/Code/extensions/EverlastEngineering.debug-in-titlebar-1.0.8/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-# Change Log
-
-All notable changes to the "debug-in-titlebar" extension will be documented in this file.
-
--- .config/Code/extensions/Willumz.generic-pseudocode-1.4.1/src/tokenTypes.ts
+++
@@ -1,8 +0,0 @@
-const tokenCodes = new Map<string, number>();
-tokenCodes.set("keyword", 0);
-
-const tokenTypesLegend = ["keyword"];
-
-const tokenModifiersLegend: string[] = [];
-
-export { tokenCodes, tokenTypesLegend, tokenModifiersLegend };
